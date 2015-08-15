cb4fbc7511482becca682ee71f8a5a9e3446772e
harish <harish.nagaraj@blackbookactivator.com>
2015-08-15 16:06:12 -0400

    Todo App Working

---
+++ src/app/todo/controllers/todocontroller.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+    angular.module('ToDo',[]).
+        controller('todoController',['$scope',function ($scope){ 
+            $scope.todos =[];
+            $scope.addTodo = function()
+            {
+                $scope.todos.push({'title':$scope.newTodo,'done':false})
+                $scope.newTodo=''
+            }
+            $scope.clearCompleted=function()
+            {
+                $scope.todos= $scope.todos.filter(function(item){
+                return !item.done
+                })
+            }
+        }])
+})();
