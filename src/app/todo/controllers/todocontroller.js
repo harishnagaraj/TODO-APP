dcc4e6d5df58ba9d2b0163e277341a893c8f1ea5
harish <harish.nagaraj@blackbookactivator.com>
2015-08-16 19:48:37 -0400

    Modified

--- src/app/todo/controllers/todocontroller.js
+++ src/app/todo/controllers/todocontroller.js
@@ -3,13 +3,27 @@
     angular.module('ToDo',[]).
         controller('todoController',['$scope',function ($scope){ 
             $scope.todos =[];
-            $scope.addTodo = function()
-            {
-                $scope.todos.push({'title':$scope.newTodo,'done':false})
-                $scope.newTodo=''
-            }
-            $scope.clearCompleted=function()
-            {
+            $scope.validationmessage='';
+            //Add Function
+            $scope.addTodo = function(){    
+                $scope.validationmessage='';      
+                if($scope.todos.length > 0 ){
+                        angular.forEach($scope.todos, function(item) 
+                        {
+                        if( item.title.toLowerCase().indexOf($scope.newTodo.toLowerCase()) >= 0 )
+                          $scope.validationmessage='Task already exists.'  
+                        else
+                        {
+                            $scope.todos.push({'title':$scope.newTodo,'done':false})
+                            $scope.newTodo=''
+                        }
+                    });}
+                else{
+                    $scope.todos.push({'title':$scope.newTodo,'done':false})
+                    $scope.newTodo='' }}
+                // Clear Function
+            $scope.finished=function()
+            {   $scope.validationmessage='';
                 $scope.todos= $scope.todos.filter(function(item){
                 return !item.done
                 })
